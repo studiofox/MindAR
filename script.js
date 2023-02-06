02abbf1899c11241b8ed686d3d5fcdb4ac6ace0a
studiofox <119687409+studiofox@users.noreply.github.com>
2023-02-06 15:38:49 +0530

    script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ dirliht.position.set(2, 2, 2);
       const loader1 = new GLTFLoader();
 
 let model1;
-loader1.load("../assets/box.glb", function (gltf) {
+loader1.load("assets/box.glb", function (gltf) {
   model1 = gltf.scene;
   anchor.group.add(model1);
   model1.position.set(-0.5, 0.2, -0.5);
